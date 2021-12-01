8116c33729fd7fd59fe71d50a6cb7e657802ff39
AlicornLunaa <alicornlunaa@gmail.com>
2021-11-30 18:52:12 -0600

    Reworked grapple hook base

--- lua/weapons/luna_grapple_base/shared.lua
+++ lua/weapons/luna_grapple_base/shared.lua
@@ -6,6 +6,7 @@ SWEP.Spawnable = false
 SWEP.ViewModel = "models/weapons/v_pistol.mdl"
 SWEP.WorldModel = "models/weapons/w_pistol.mdl"
 
+-- Ammo
 SWEP.Primary.ClipSize = -1
 SWEP.Primary.DefaultClip = -1
 SWEP.Primary.Automatic = false
@@ -24,67 +25,56 @@ SWEP.weaponColor = Color(255, 255, 255)
 
 -- Functions
 function SWEP:Think()
-    -- Serverside code
-    if SERVER then
-        -- Only run when the game is running
-        if !self:IsValid() then return end
-        if game.SinglePlayer() and gameUIVisible then return end
+    -- This function will reel the hook in depending on the direction given
+    self:Reel()
+end
 
-        -- Get variables
-        local reeling = self:GetNWBool("reeling", false)
-        local expanding = self:GetNWBool("expanding", false)
+function SWEP:Reel()
+    -- This function starts reeling in the direction supplied
+    -- Error checking
+    if !SERVER then return end
+    if game.SinglePlayer() and gameUIVisible then return end
+    if !self.hook or !self.hook:IsValid() then return end
+    if self.direction == 0 then return end
 
-        -- Reduce the targeted distance
-        if reeling and self:GetOwner():IsValid() then
-            self.hook.targetDistance = math.Clamp(self.hook.targetDistance - self.reelSpeed, 1, self.hook.maxDistance)
-
-            -- Check for a continuous hold of the reel button
-            if !self:GetOwner():KeyDown(IN_ATTACK) then
-                -- Primary attack was let go, stop reeling
-                self:SetNWBool("reeling", false)
-                self:StopSound("reel_sound")
-            end
-        end
-
-        -- Increase the targeted distance
-        if expanding and self:GetOwner():IsValid() then
-            self.hook.targetDistance = math.Clamp(self.hook.targetDistance + self.reelSpeed, 1, self.hook.maxDistance)
-
-            -- Check for a continuous hold of the reel button
-            if !self:GetOwner():KeyDown(IN_ATTACK2) then
-                -- Secondary attack was let go, stop expanding
-                self:SetNWBool("expanding", false)
-                self:StopSound("reel_sound")
-            end
-        end
-    end
+    -- Start changing values
+    self.hook.targetDistance = math.Clamp(self.hook.targetDistance - self.reelSpeed * self.direction, 1, self.hook.maxDistance)
 end
 
 function SWEP:Initialize()
     -- Create model to render on gun
-    if CLIENT then
-        local ent = ents.CreateClientside(self.hookClass)
-        ent:SetNoDraw(true)
+    self.direction = 0
 
-        self.hookMdl = ent
-    end
+    -- Only create model on client
+    if !CLIENT then return end
+    local ent = ents.CreateClientside(self.hookClass)
+    ent:SetNoDraw(true)
+    self.hookMdl = ent
 end
 
 function SWEP:Cleanup()
     -- Detaches the hook
+    -- Error checking
     if !SERVER then return end
-    if !self.hook or !self.hook.hookAttached then return end
+    if !self.hook or !self.hook:IsValid() then return end
 
-    -- Play sounds
-    self:EmitSound("release_sound")
-    self:StopSound("reel_sound")
+    -- Sounds
+    if self:GetOwner():IsValid() then
+        self:GetOwner():StopSound("reel_sound")
+        self:GetOwner():EmitSound("release_sound")
+    end
+
+    -- Make sure the hook is detached from forces
     self.hook:SetHookAttached(false)
+
+    -- Keep reference for deletion
+    local _hook = self.hook
+    self.hook = NULL
 
     -- Remove hook after 3 seconds
     timer.Simple(3, function()
-        if self.hook and self.hook:IsValid() then
-            self.hook:Remove()
-        end
+        if !_hook or !_hook:IsValid() then return end
+        _hook:Remove()
     end )
 end
 
@@ -96,51 +86,74 @@ end
 function SWEP:PrimaryAttack()
     -- This function will launch the hook and wait for it to grapple
     -- or it will reel the grapple in depending on the status of it
-    -- Store data in variables
+    if !SERVER then return end
     if !self:GetOwner():IsValid() then return end
 
-    -- Save data
-    local lookDirection = self:GetOwner():GetAimVector()
+    -- Collect variables
+    local ply = self:GetOwner()
+    local vm = ply:GetViewModel()
+    local lookDirection = ply:GetAimVector()
 
-    -- Run functions
-    if self.hook and self.hook.hookAttached then
-        -- Hook already launched, reel it in if its hooked.
-        self:SetNWBool("reeling", true)
-        self:EmitSound("reel_sound")
-    else
+    -- Logic
+    if self.hook and self.hook:IsValid() and self.hook.hookAttached then
+        -- Hook already launched, reel it in
+        ply:EmitSound("reel_sound")
+        self.direction = 1
+
+        -- Create hook to wait for end
+        hook.Add("KeyRelease", "hookReelEnd" .. tostring(self.hook:EntIndex()), function(_ply, key)
+            -- Error checking
+            if _ply != ply then return end
+            if key != IN_ATTACK then return end
+            if !self:IsValid() or !self:GetOwner():IsValid() then return end
+
+            -- Stop reel
+            self.direction = 0
+            ply:StopSound("reel_sound")
+
+            -- Stop listening for a release
+            hook.Remove("KeyRelease", "hookReelEnd" .. tostring(self.hook:EntIndex()))
+        end )
+    elseif !self.hook or !self.hook:IsValid() or !self.hook.hookAttached then
         -- Hook has not been launched, launch it.
         self:SetNextPrimaryFire(CurTime() + 0.2)
-        self:EmitSound("firing_sound")
         self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
+        ply:EmitSound("firing_sound")
 
-        -- Serverside only
-        if SERVER then
-            -- Spawn the hook at hand and launch it in the look direction
-            local viewModel = self:GetOwner():GetViewModel()
-            local attachmentPoint = self:GetAttachment(1)
+        -- Get positional data
+        local boneTransform = self:GetAttachment(1)
 
+        if vm:IsValid() and boneTransform then
+            -- Spawn the hook at the end of the muzzle
             local ent = ents.Create(self.hookClass)
-            ent:SetPos(attachmentPoint.Pos + viewModel:GetForward() * ent.positionOffset.x)
-            ent:SetAngles(viewModel:LocalToWorldAngles(ent.angleOffset))
+            ent:SetPos(boneTransform.Pos + vm:GetForward() * ent.positionOffset.x)
+            ent:SetAngles(vm:LocalToWorldAngles(ent.angleOffset))
             ent:Spawn()
             ent:SetHookLauncher(self:GetOwner())
             ent:SetColor(self.weaponColor)
+
+            -- Launch the hook
             ent:GetPhysicsObject():ApplyForceCenter(lookDirection * self.launchForce)
 
+            -- Save references to new data
             self.hook = ent
             self:SetNWEntity("hook", ent)
 
             -- Setup the launch function to activate once the key is released
-            hook.Add("KeyRelease", "hookLaunchActive", function(ply, key)
-                if key == IN_ATTACK and self:IsValid() and self:GetOwner():IsValid() then
-                    local distance = self:GetOwner():GetPos():Distance(ent:GetPos())
+            hook.Add("KeyRelease", "hookLaunchActive" .. tostring(ent:EntIndex()), function(_ply, key)
+                -- Error checking
+                if _ply != ply then return end
+                if key != IN_ATTACK then return end
+                if !self:IsValid() or !self:GetOwner():IsValid() then return end
 
-                    ent.lastDistance = math.Clamp(distance, 1, ent.maxDistance)
-                    ent.targetDistance = math.Clamp(distance, 1, ent.maxDistance)
-                    ent:SetHookAttached(true)
-                end
+                -- Send data to the hook
+                local distance = self:GetOwner():GetPos():Distance(ent:GetPos())
+                ent.lastDistance = math.Clamp(distance, 1, ent.maxDistance)
+                ent.targetDistance = math.Clamp(distance, 1, ent.maxDistance)
+                ent:SetHookAttached(true)
 
-                hook.Remove("KeyRelease", "hookLaunchActive")
+                -- Stop listening for a release
+                hook.Remove("KeyRelease", "hookLaunchActive" .. tostring(ent:EntIndex()))
             end )
         end
     end
@@ -148,66 +161,85 @@ end
 
 function SWEP:SecondaryAttack()
     -- This function will cause the hook to expand
-    if SERVER and self.hook.hookAttached then
-        self:SetNWBool("expanding", true)
-        self:EmitSound("reel_sound")
-    end
+    if !SERVER then return end
+    if !self.hook or !self.hook:IsValid() or !self.hook.hookAttached then return end
+    if !self:GetOwner() then return end
+
+    -- Expand the book
+    local ply = self:GetOwner()
+    ply:EmitSound("reel_sound")
+    self.direction = -1
+
+    -- Create hook to wait for end
+    hook.Add("KeyRelease", "hookExpandEnd" .. tostring(self.hook:EntIndex()), function(_ply, key)
+        -- Error checking
+        if _ply != ply then return end
+        if key != IN_ATTACK2 then return end
+        if !self:IsValid() or !self:GetOwner():IsValid() then return end
+
+        -- Stop reel
+        self.direction = 0
+        ply:StopSound("reel_sound")
+
+        -- Stop listening for a release
+        hook.Remove("KeyRelease", "hookExpandEnd" .. tostring(self.hook:EntIndex()))
+    end )
 end
 
 function SWEP:ViewModelDrawn(ent)
-    if CLIENT then
-        -- Draw the beam for the viewmodel
-        -- Get networked information
-        if !self:GetOwner():IsValid() or !ent:IsValid() or !self.hookMdl:IsValid() then return end
+    -- Draw the beam for the viewmodel
+    if !CLIENT then return end
+    if !self:GetOwner():IsValid() then return end
+    if !ent:IsValid() then return end
+    if !self.hookMdl:IsValid() then return end
 
-        -- Save data
-        local vm = self:GetOwner():GetViewModel()
-        local attachmentPoint = vm:GetAttachment(1)
-        local _hook = self:GetNWEntity("hook", NULL)
+    -- Get information to render
+    local vm = self:GetOwner():GetViewModel()
+    local attachmentPoint = vm:GetAttachment(1)
+    local _hook = self:GetNWEntity("hook", NULL)
 
-        ent:SetColor(self.weaponColor)
-        self.hookMdl:SetColor(self.weaponColor)
+    -- Make the color match the setting
+    ent:SetColor(self.weaponColor)
+    self.hookMdl:SetColor(self.weaponColor)
 
-        -- Get location to attach to
-        if !_hook:IsValid() or !_hook:GetHookAttached() then
-            -- Draw hook on the gun because it's not launched
-            cam.Start3D()
-                local pos, ang = LocalToWorld(self.hookMdl.positionOffset, self.hookMdl.angleOffset + Angle(-10, 90, 90), attachmentPoint.Pos, attachmentPoint.Ang)
-                self.hookMdl:SetRenderOrigin(pos)
-                self.hookMdl:SetRenderAngles(ang)
-                self.hookMdl:SetPos(pos)
-                self.hookMdl:SetAngles(ang)
-                self.hookMdl:DrawModel()
-            cam.End3D()
-        end
-    end
-end
-
-function SWEP:DrawWorldModel(flags)
-    if CLIENT then
-        -- Draw the gun and rope to the hook
-        self:DrawModel(flags)
-
-        -- Get networked information
-        local attachmentPoint = self:GetAttachment(1)
-
-        -- Get location to attach to
-        if self.hookMdl:IsValid() and attachmentPoint then
-            -- Draw the hook since there is none launched
-            local pos, ang = LocalToWorld(self.hookMdl.positionOffset, self.hookMdl.angleOffset, attachmentPoint.Pos, attachmentPoint.Ang)
+    -- Get location to attach to
+    if !_hook:IsValid() or !_hook:GetHookAttached() then
+        -- Draw hook on the gun because it's not launched
+        cam.Start3D()
+            local pos, ang = LocalToWorld(self.hookMdl.positionOffset, self.hookMdl.angleOffset + Angle(-10, 90, 90), attachmentPoint.Pos, attachmentPoint.Ang)
             self.hookMdl:SetRenderOrigin(pos)
             self.hookMdl:SetRenderAngles(ang)
             self.hookMdl:SetPos(pos)
             self.hookMdl:SetAngles(ang)
             self.hookMdl:DrawModel()
-        end
+        cam.End3D()
+    end
+end
+
+function SWEP:DrawWorldModel(flags)
+    -- Draw the gun and rope to the hook
+    if !CLIENT then return end
+
+    -- Make sure the default model is rendered
+    self:DrawModel(flags)
+
+    -- Get positional data for the hook
+    local boneTransform = self:GetAttachment(1)
+
+    -- Get location to attach to
+    if self.hookMdl:IsValid() and boneTransform then
+        -- Draw the hook since there is none launched
+        local pos, ang = LocalToWorld(self.hookMdl.positionOffset, self.hookMdl.angleOffset, boneTransform.Pos, boneTransform.Ang)
+        self.hookMdl:SetRenderOrigin(pos)
+        self.hookMdl:SetRenderAngles(ang)
+        self.hookMdl:SetPos(pos)
+        self.hookMdl:SetAngles(ang)
+        self.hookMdl:DrawModel()
     end
 end
 
 function SWEP:Holster()
     -- Weapon was holstered, fix colors
-    self:Cleanup()
-
     if self:IsValid() and self:GetOwner():IsValid() and self:GetOwner():GetViewModel():IsValid() then
         self:GetOwner():GetViewModel():SetColor(Color(255, 255, 255))
     end
@@ -216,6 +248,12 @@ function SWEP:Holster()
 end
 
 function SWEP:OnRemove()
-    if CLIENT and self.hookMdl:IsValid() then self.hookMdl:Remove() end
+    -- Weapon was removed, make sure to delete the clientside models
+    -- and to detach the hook
+    if CLIENT and self.hookMdl:IsValid() then
+        self.hookMdl:Remove()
+    end
+
+    self:Cleanup()
     self:Holster()
 end
